86ecaf8f7711b989383acbafc64ef9ae5e4f919a
Ousman <usman.traid@gmail.com>
2023-04-19 03:41:44 -0400

    all madatory tasks completed

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,10 +1,10 @@
 export default function guardrail(mathFunction) {
-    const arr = []
-    try {
-        arr.push(mathFunction())
-    } catch (e) {
-        arr.push(e.toString())
-    }
-    arr.push('Guardrail was processed')
-    return arr
+  const arr = [];
+  try {
+    arr.push(mathFunction());
+  } catch (e) {
+    arr.push(e.toString());
+  }
+  arr.push('Guardrail was processed');
+  return arr;
 }
