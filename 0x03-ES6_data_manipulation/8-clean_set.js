ddb1ad61052a8b756ef919e03f7711192f42829c
Ousman <usman.traid@gmail.com>
2023-04-25 08:07:19 -0400

    eslint requirement fixed

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (!set || !startString || typeof startString !== 'string' || !(set instanceof Set)) return '';
 
   return [...set]
-    .filter((ele) => typeof ele == 'string' && ele.startsWith(startString))
+    .filter((ele) => typeof ele === 'string' && ele.startsWith(startString))
     .map((ele) => ele.slice(startString.length))
     .join('-');
 }
