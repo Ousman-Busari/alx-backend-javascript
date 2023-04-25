a8d4288e49dd47330c51aba67c7a97bc16860ff4
Ousman <usman.traid@gmail.com>
2023-04-25 07:42:42 -0400

    advanced task completed

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,8 +3,8 @@ export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
   }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
   if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
-  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
 }
