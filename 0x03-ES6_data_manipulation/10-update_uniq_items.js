10467a51ab35e90a060fd3200ce1ccea720071bd
Ousman <usman.traid@gmail.com>
2023-04-25 05:36:23 -0400

    eslint requirement fixed

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,6 @@
 export default function updateUniqueItems(groceries) {
   if (!(groceries instanceof Map)) {
-    throw Error("Cannot process");
+    throw Error('Cannot process');
   }
 
   for (const [key, value] of groceries) {
