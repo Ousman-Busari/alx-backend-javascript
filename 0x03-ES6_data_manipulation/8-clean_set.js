2fb3c102bd7c61cdc12774bc4f79421353e14481
Ousman <usman.traid@gmail.com>
2023-04-25 08:04:01 -0400

    code refactored

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!ser || !startString || typeof startString !== 'string' || !(set instanceof Set)) return '';
+  if (!set || !startString || typeof startString !== 'string' || !(set instanceof Set)) return '';
 
   return [...set]
     .filter((ele) => ele.startsWith(startString))
