51ca12cd3fd8f1bc552c9e0f0137bf6e68f47ef6
Ousman <usman.traid@gmail.com>
2023-04-25 07:55:47 -0400

    code refactored

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,20 @@
-export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
+// export default function cleanSet(set, startString) {
+//   if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
 
+//   return [...set]
+//     .filter((ele) => ele.startsWith(startString))
+//     .map((ele) => ele.slice(startString.length))
+//     .join('-');
+// }
+
+export default function cleanSet(set, startString) {
+  if (
+    !startString
+    || typeof startString !== 'string'
+    || typeof set !== 'object'
+  ) return '';
   return [...set]
-    .filter((ele) => ele.startsWith(startString))
-    .map((ele) => ele.slice(startString.length))
+    .filter((el) => el.startsWith(startString))
+    .map((el) => el.slice(startString.length))
     .join('-');
 }
