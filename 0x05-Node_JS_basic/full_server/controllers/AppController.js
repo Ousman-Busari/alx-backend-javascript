23abbc15097f4a6876b1281d491d25c418db461f
Ousman <usman.traid@gmail.com>
2023-06-13 14:12:47 -0400

    content-type header removed from reponse

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,6 +1,5 @@
 class AppController {
   static getHome(request, response) {
-    response.setHeader('Content-Type', 'text/plain');
     response.status(200).send('Hello Holberton School!');
   }
 }
