bfd850594ab15c9c33adf2fa07c63c596cf0be09
Ousman <usman.traid@gmail.com>
2023-06-15 18:03:23 -0400

    not found page status code added to test suite

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -3,21 +3,35 @@ const request = require("request");
 
 describe("express app integration tests", () => {
   describe("Index page", () => {
-    const options = {
-      url: "http://localhost:7865",
-      method: "GET",
-    };
-
     it("should return 200 status code", (done) => {
+      const options = {
+        url: "http://localhost:7865",
+        method: "GET",
+      };
       request(options, (err, res, body) => {
         expect(res.statusCode).to.equal(200);
       });
       done();
     });
-
+    
     it("should return 'Welcome to the payment system as' as Body", (done) => {
+      const options = {
+        url: "http://localhost:7865",
+        method: "GET",
+      };
       request(options, (err, res, body) => {
         expect(body).to.equal("Welcome to the payment system");
+      });
+      done();
+    });
+
+    it("should return 200 status code", (done) => {
+      const options = {
+        url: "http://localhost:7865/not_found",
+        method: "GET",
+      };
+      request(options, (err, res, body) => {
+        expect(res.statusCode).to.equal(404);
       });
       done();
     });
